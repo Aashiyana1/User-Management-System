d3629b97dcc47f406a37b5d5e27fafdcd98140d7
Aashiyana1 <84777704+Aashiyana1@users.noreply.github.com>
2025-05-09 17:07:38 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUDAPP.Model
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public string DateOfBirth { get; set; }
+        public decimal BasicSalary { get; set; }
+
+    }
+}
